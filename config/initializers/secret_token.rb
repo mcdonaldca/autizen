0510a8fea1b54db4486c1abd7ce11822deef09ef
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2015-01-03 15:53:27 -0500

    Initialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Autizen::Application.config.secret_key_base = '36ab254235ebc0c24f556d8b74b9c3578943520e72555b83e8058c37481eb1d3ddd87b0df46694e0754ce377e271768fbe9b8f8d674deaf50a12cd298ea93dc3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Autizen::Application.config.session_store :cookie_store, key: '_autizen_session'
